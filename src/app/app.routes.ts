f81489386d9676296b7475f45ac9afc14c48968d
Amira Nasri <nasriamira88@gmail.com>
2024-04-16 22:40:27 +0100

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { WhoWeAreComponent } from './pages/partials/who-we-are/who-we-are.component';
 
 export const routes: Routes = [
+
     { path: '', component: HomeComponent },
+    { path: 'how-it-works', component: WhoWeAreComponent },
+    { path: 'faq', component: FaqComponent },
+    { path: 'auth', component: AuthComponent },
+    { path: 'signup', component: SignupComponent },
    
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
+  imports: [RouterModule,MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
